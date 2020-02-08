81dd1f333a45451a0fa7a5bd4d658d891a170e74
passby_time <passby_time@naver.com>
2020-02-08 15:26:33 +0900

    toDos array 생성 및 toDoObj에서 입력한 text와 id값 가져오기
    이름이 toDos인 array 생성(할일 목록을 여기에다가 저장)
    toDoObj에는 입력한 text와 id가 들어감
    새롭게 생긴 newId는 toDos.length + 1
    
    loadToDos 함수안의 변수 명 변경(헷갈릴 수 있어서)

--- create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/todo.js
+++ create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/todo.js
@@ -4,15 +4,25 @@ const toDoFrom = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
   span.innerText = text;
-  li.appendChild(span);
   li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId
+  }
+  toDos.push(toDoObj);
 }
 
 function handleSubmit(event) {
@@ -23,8 +33,8 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
   }
 }
 
