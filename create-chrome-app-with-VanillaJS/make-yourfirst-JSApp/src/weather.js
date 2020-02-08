886b79e02f4690fb7a9261d26fdddf003b1b3000
passby_time <passby_time@naver.com>
2020-02-09 01:06:04 +0900

    getWeather로 위도 경도 가져오기(API_KEY를 통해)
    getWeather로 위도와 경도 가져오기
    openweathermap에서 API를 가져올 수 있다.

--- create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/weather.js
+++ create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/weather.js
@@ -1,4 +1,19 @@
+const API_KEY = "eba320146f77769ac4410cba939d8f5f"; // https://home.openweathermap.org/api_keys 에서 값 복사해옴
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&unit=metric`
+  ).then(function(response){
+    return response.json();
+  }).then(function(json){
+    console.log(json)
+  });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
 
 function handleGeoSucces(position) {
   const latitude = position.coords.latitude;
@@ -7,7 +22,8 @@ function handleGeoSucces(position) {
     latitude,
     longitude
   };
-
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -15,7 +31,7 @@ function handleGeoError() {
 }
 
 function askForCoords() {
-  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError); 
 }
 
 function loadCoords() {
@@ -23,7 +39,8 @@ function loadCoords() {
   if(loadedCoords === null) {
     askForCoords();
   } else {
-    // getWeather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
