f8ff6bc8ddbaf082fa3ab51738ef5cf7d6359f1b
passby_time <passby_time@naver.com>
2020-02-09 00:18:49 +0900

    askForCoords 함수 추가
    handleGeoSucces, handleGeoError 함수를 추가함

--- create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/weather.js
+++ create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/weather.js
@@ -1,8 +1,26 @@
 const COORDS = 'coords';
 
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+
+}
+
+function handleGeoError() {
+  console.log('Cant access geo location');
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
 function loadCoords() {
   const loadedCoords = localStorage.getItem(COORDS);
-  if(loadCoords === null) {
+  if(loadedCoords === null) {
     askForCoords();
   } else {
     // getWeather
