48fa5416bb66e98839197c76f33fe580192dbdfc
passby_time <passby_time@naver.com>
2020-02-08 16:42:45 +0900

    새로고침하면 List 사라지게 하기
    X 버튼을 눌러서 List를 사라지게 해도 새로고침을 하면 남아있음. cleanToDos 함수를 사용해 새로고침하면 없어지는 기능을 추가함.
    filter는 array의 모든 item을 통해 함수를 실행하고 true인 아이템들만 가지고 새로운 array를 만든다.

--- create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/todo.js
+++ create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/todo.js
@@ -4,12 +4,17 @@ const toDoFrom = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
 
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
   toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
 }
 
 function saveToDos() {
