ad6f04355093d169c0c35738bcd91acd39a494f7
passby_time <passby_time@naver.com>
2020-02-08 17:18:48 +0900

    paintImage 추가
    이미지를 랜덤으로 가져옴. 오타 수정.

--- create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/bg.js
+++ create-chrome-app-with-VanillaJS/make-yourfirst-JSApp/src/bg.js
@@ -2,13 +2,20 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`;
+  body.appendChild(image);
+}
+
 function getRandom() {
   const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
 }
 
 function init() {
-  const rendomNumber = getRandom();
+  const randomNumber = getRandom();
+  paintImage(randomNumber)
 }
 
 init();
