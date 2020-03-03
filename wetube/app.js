60f02f34e27c7fc86d87795107d6274da076e383
passby_time <passby_time@naver.com>
2020-03-04 04:07:02 +0900

    router 사용
    router를 이용하여 모든 것을 작은 단위로 쪼개 사용
    route 들의 복잡함을 쪼개주는데 사용할 수 있다.

--- wetube/app.js
+++ wetube/app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
 const app = express();
 
 const handleHome = (req, res) => res.send("Hello from home!!");
@@ -18,5 +20,7 @@ app.use(morgan("dev"));
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
+
+app.use("/user", userRouter);
 
 export default app;
