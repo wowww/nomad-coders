112f5a29cf89ff0ba77ad14f45b3bdedc5fefe11
passby_time <passby_time@naver.com>
2020-03-04 01:32:56 +0900

    Middleware, cookie-parser, body-parser 설치
    1. Middleware - Morgan 설치
    - logging에 도움을 준다. 무슨 일이 일어났는지 기록
    2. Middleware - Helmet 설치
    - NodeJS 앱 보안을 위한 것
    3. cookie-parser, body-parser 설치

--- wetube/index.js
+++ wetube/index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -9,14 +13,13 @@ const handleHome = (req, res) => res.send("Hello from home!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-  console.log('Between');
-  next();
-}
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.use(betweenHome);
-
-app.get('/',betweenHome , handleHome);
+app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
 
