6b90dbd7214a67bc03180c294d9d9e60dcea3eea
passby_time <passby_time@naver.com>
2020-03-03 22:54:49 +0900

    package.json delay추가 및 index.js에 middleware 추가
    1. package.json delay추가
    2초후 실행되도록 delay 추가
    2. 전역에 middleware 추가
    참고: https://expressjs.com/ko/guide/using-middleware.html

--- wetube/index.js
+++ wetube/index.js
@@ -9,7 +9,14 @@ const handleHome = (req, res) => res.send("Hello from home!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.get('/', handleHome);
+const betweenHome = (req, res, next) => {
+  console.log('Between');
+  next();
+}
+
+app.use(betweenHome);
+
+app.get('/',betweenHome , handleHome);
 
 app.get('/profile', handleProfile);
 
