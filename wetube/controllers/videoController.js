f4a80582be78e5ed3421048682a93c269845ae7b
passby_time <passby_time@naver.com>
2020-03-05 02:28:24 +0900

    Pug 설치, Controller 내용 추가
    Pug는 express에서 View를 다루는 방식 중 하나임
    HTML을 멋지게 보이도록 해줌

--- wetube/controllers/videoController.js
+++ wetube/controllers/videoController.js
@@ -1,2 +1,7 @@
-export const home = (req, res) => res.send("Home");
+export const home = (req, res) => res.render("Home");
 export const search = (req, res) => res.send("Search");
+export const videos = (req, res) => res.send("Videos");
+export const upload = (req, res) => res.send("Upload");
+export const videosDetail = (req, res) => res.send("Video Detail");
+export const editVideos = (req, res) => res.send("Edit Video");
+export const deleteVideos = (req, res) => res.send("Delete Video");
