e32af7484f781482c7c2cc1f46aa670d109176ff
passby_time <passby_time@naver.com>
2020-03-07 02:44:32 +0900

    Create db.js
    샘플 데이터 베이스 추가

---
+++ wetube/db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: 'Video awesome',
+    description: 'This is somthing I love',
+    views: 24,
+    videoFile: "httis://acrchive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Lia",
+      email: "passby_time@naver.com"
+    }
+  },
+  {
+    id: 1212122,
+    title: 'Video super',
+    description: 'This is somthing I love',
+    views: 24,
+    videoFile: "httis://acrchive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Lia",
+      email: "passby_time@naver.com"
+    }
+  },
+  {
+    id: 34343434,
+    title: 'Video nice',
+    description: 'This is somthing I love',
+    views: 24,
+    videoFile: "httis://acrchive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Lia",
+      email: "passby_time@naver.com"
+    }
+  },
+  {
+    id: 11111,
+    title: 'Video perfect',
+    description: 'This is somthing I love',
+    views: 24,
+    videoFile: "httis://acrchive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Lia",
+      email: "passby_time@naver.com"
+    }
+  }
+]
