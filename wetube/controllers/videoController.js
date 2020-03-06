110d83f8815f3ea5f9bb6199a9339db79f31f73a
passby_time <passby_time@naver.com>
2020-03-07 02:42:59 +0900

    오타 수정, 경로 변경

--- wetube/controllers/videoController.js
+++ wetube/controllers/videoController.js
@@ -1,4 +1,8 @@
-export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
+import { videos } from "../db" 
+
+export const home = (req, res) => {
+res.render("Home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
   const {
@@ -7,8 +11,8 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy });
 };
 
-export const videos = (req, res) => res.render("Videos", { pageTitle: "Videos" });
-export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
-export const videosDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail" });
-export const editVideos = (req, res) => res.render("Edit Video", { pageTitle: "Edit Video" });
-export const deleteVideos = (req, res) => res.render("Delete Video", { pageTitle: "Delete Video" });
+// export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Videos" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Videos" });
