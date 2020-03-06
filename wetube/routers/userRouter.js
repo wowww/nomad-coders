46149dd7012185f763aa6c27498ebf0a6c6e26b5
passby_time <passby_time@naver.com>
2020-03-07 01:41:57 +0900

    editProfile, join, login, userRouter 수정

--- wetube/routers/userRouter.js
+++ wetube/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
 import routes from "../routes";
+	
+import { 
+  editProfile, 
+  userDetail, 
+  changePassword 
+} from "../controllers/userController";
+
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req, res) => res.send("Change Password"));
+// userRouter.get(routes.users, (req, res) => res.send("Users"));
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
