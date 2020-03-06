bb5b4dbe678f3ff06409029bc582c44c9c97b4d8
passby_time <passby_time@naver.com>
2020-03-06 18:09:37 +0900

    Search Controller
    header.pug에 input창으로 검색 페이지 구현

--- wetube/controllers/videoController.js
+++ wetube/controllers/videoController.js
@@ -1,7 +1,14 @@
-export const home = (req, res) => res.render("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videosDetail = (req, res) => res.send("Video Detail");
-export const editVideos = (req, res) => res.send("Edit Video");
-export const deleteVideos = (req, res) => res.send("Delete Video");
+export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const videos = (req, res) => res.render("Videos", { pageTitle: "Videos" });
+export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
+export const videosDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail" });
+export const editVideos = (req, res) => res.render("Edit Video", { pageTitle: "Edit Video" });
+export const deleteVideos = (req, res) => res.render("Delete Video", { pageTitle: "Delete Video" });
