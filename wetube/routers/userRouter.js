e79d0f611c2edb8b03a0d1be35e41b9a015d8a90
passby_time <passby_time@naver.com>
2020-03-07 06:06:38 +0900

    Login and User Profile Controlers

--- wetube/routers/userRouter.js
+++ wetube/routers/userRouter.js
@@ -11,7 +11,7 @@ const userRouter = express.Router();
 
 // userRouter.get(routes.users, (req, res) => res.send("Users"));
 userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
