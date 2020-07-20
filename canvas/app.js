ab6a12bc78a2092c6a67f8a8a1df1f79d208f51d
passby_time <passby_time@naver.com>
2020-07-20 18:02:59 +0900

    feat. 저장될 때 기본 바탕 이미지가 투명이 아닌 흰색(white)이 되도록 기능 추가
    따로 지정하지 않으면 png로 저장했을 때 투명색(transparent)으로 저장됨
    이를 막기 위해 미리 배경색을 흰색(white)으로 지정함

--- canvas/app.js
+++ canvas/app.js
@@ -10,6 +10,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
